1fcf68002a7d719f7a0bfa85b522a8a7e4803fa8
victorhqc <victorhqc@gmail.com>
2013-11-14 12:55:40 -0700

    Webapp created

---
+++ modules/login/js/main.js
@@ -0,0 +1,48 @@
+(function(App){
+	"use strict";
+	var Module = function(App){
+		this.initialFunctionality();
+		this.a = App;
+	}
+
+	Module.prototype.initialFunctionality = function() {
+		var form = document.getElementById('form-login');
+
+		var T = this;
+		form.addEventListener('submit', function(e){
+			if(e.preventDefault){
+				e.preventDefault();
+			}
+			e.returnValue = false;
+
+			T.sendRequest();
+		}, false);
+	};
+
+	Module.prototype.sendRequest = function(){
+		var btn = document.getElementById('login-button');
+		btn.setAttribute('disabled', 'disabled');
+
+		var data = {};
+		var inputs = document.querySelectorAll('#form-login input');
+		for(var i = 0, len = inputs.length; i < len; i++){
+			var input = inputs[i];
+			var id = input.id;
+			data[id] = input.value;
+		}
+
+		/*new Vi({url:'verifyEntry.php', data:data, response:'object'}).server(function(r){
+			btn.removeAttribute('disabled');
+			if(r.success === true){
+				//User is redirected to the backend of the platform.
+				Application.loadCategory('backend');
+			}else{
+				//Error is shown to user
+				var e = document.getElementById('errors');
+				e.style.display = 'block';
+			}
+		});*/
+	}
+
+	var i = new Module(App);
+})(App);
