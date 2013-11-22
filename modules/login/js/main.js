ee91f799e900d5fe1dbf1cef0f707e69af639e72
victorhqc <victorhqc@gmail.com>
2013-11-22 09:08:14 -0700

    Revamped login
    Adapted to the new OAuth authentication method. This application no
    longer handles username and passwords by itself.

--- modules/login/js/main.js
+++ modules/login/js/main.js
@@ -3,19 +3,15 @@
 	var Module = function(App){
 		this.initialFunctionality();
 		this.a = App;
+		var t = this;
 	}
 
 	Module.prototype.initialFunctionality = function() {
-		var form = document.getElementById('form-login');
+		var btn = document.getElementById('login-button');
 
-		var T = this;
-		form.addEventListener('submit', function(e){
-			if(e.preventDefault){
-				e.preventDefault();
-			}
-			e.returnValue = false;
-
-			T.sendRequest();
+		btn._t = this;
+		btn.addEventListener('click', function(e){
+			this._t.sendRequest();
 		}, false);
 	};
 
@@ -23,35 +19,55 @@
 		var btn = document.getElementById('login-button');
 		btn.setAttribute('disabled', 'disabled');
 
-		var data = {};
-		var inputs = document.querySelectorAll('#form-login input');
-		for(var i = 0, len = inputs.length; i < len; i++){
-			var input = inputs[i];
-			var id = input.id;
-			data[id] = input.value;
-		}
-
-
-		var j = {
-			url: this.a._data.rest+'Members/',
-			mode: 'POST',
-			div: undefined,
-			cache: true,
-			data: data,
-			response: 'object'
-		}
-		new Vi(j).ajax(function(r){
+		var t = this;
+		this.verifyIdentity(function(r){
 			btn.removeAttribute('disabled');
-			if(r.success === true){
-				//User is redirected to the backend of the platform.
-				Application.loadCategory('backend');
-			}else{
-				//Error is shown to user
-				var e = document.getElementById('errors');
-				e.style.display = 'block';
-			}
+			t.handleServerResponse(r);
 		});
 	}
+
+	Module.prototype.handleServerResponse = function(r) {
+		if(r.success === true){
+			//User is redirected to the backend of the platform.
+			Application.loadCategory('backend');
+		}else{
+			//Error is shown to user
+			/*var e = document.getElementById('errors');
+			e.style.display = 'block';*/
+		}
+	};
+
+	Module.prototype.verifyAccess = function(callback) {
+		$.oajax({
+			url: this.a._data.rest+'Members/',
+			jso_provider: "uvicate",
+			jso_allowia: false,
+			jso_scopes: ["profile"],
+			dataType: 'json',
+			success: function(data) {
+				callback(data);
+			}
+		});
+	};
+
+	Module.prototype.verifyIdentity = function(callback) {
+		jso_ensureTokens({
+			"uvicate": []
+		});
+
+		jso_dump();
+
+		$.oajax({
+			url: this.a._data.rest+'Members/',
+			jso_provider: "uvicate",
+			jso_allowia: true,
+			jso_scopes: ["profile"],
+			dataType: 'json',
+			success: function(data) {
+				callback(data);
+			}
+		});
+	};
 
 	var i = new Module(App);
 })(App);
